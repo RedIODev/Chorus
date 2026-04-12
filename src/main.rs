e7cccbf2f463c18b4ed5a8f96f06795e1cb9d0bc
RedIODev <redio.development@gmail.com>
2026-04-12 22:42:26 +0200

    updated file loading and keyword parsing, added benchmarking and cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,29 @@
-use std::{error::Error, fs::File};
+use std::error::Error;
 
-use crate::tokenizer::{Keyword, Tokenizer};
+use crate::tokenizer::{Tokenizer};
 
 mod tokenizer;
 
+
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 fn main() -> TmpResult<()> {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
 
     let mut token_len = 0;
     let mut token_count = 0;
-    for _ in 0..10_000 {
-        let tokenizer = Tokenizer::new("./Testfile.ch")?;
-        for token in tokenizer {
-            token_len += token.token.line_len();
-            token_count+=1;
-        }
-    }
-    println!("Token_len:{token_len}, Token_count:{token_count}");
-    // let tokenizer = Tokenizer::new("./Testfile.ch")?;
-    // for token in tokenizer {
-    //     println!("Token:[{:?}]", token)
-    // }
-
-    // println!();
-    // let tokenizer = Tokenizer::new("./Testfile.ch")?;
-    // let mut ll = 0;
-    // for token in tokenizer {
-    //     use tokenizer::Token;
-    //     if ll != token.line {
-    //         println!();
-    //     }
-    //     match token.token {
-    //         Token::Comment(c) => print!("{} ", c.comment()),
-    //         Token::Identifier(i) => print!("{i} "),
-    //         Token::Keyword(k) => print!("{} ", <&str>::from(k)),
-    //         Token::Symbol(s) => print!("{:?} ", s as u8 as char)
-    //     }
-    //     ll = token.line;
-    // }
-    // println!();
+    let tokenizer = Tokenizer::new("./Testfile.ch")?;
+            for token in tokenizer {
+                token_len += token.token.line_len();
+                token_count+=1;
+                if token_count < 4000 {
+                    println!("Token:[{:?}]", token)
+                }
+            }
+println!("Token_len:{token_len}, Token_count:{token_count}");
         Ok(())
 }
 
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,41 +1,45 @@
-use std::collections::BTreeMap;
-use std::iter::FilterMap;
+use std::collections::HashMap;
 use std::ops::{Add, AddAssign};
-use std::{fs::File, path::Path};
-use std::io::{self, Bytes, Read};
+use std::process::exit;
 
-use itertools::PeekNth;
+use std::{fs::File, path::Path};
+
+use itertools::{Itertools, MultiPeek};
 use lazy_static::lazy_static;
+use memmap2::{Mmap, MmapOptions};
 use num_enum::TryFromPrimitive;
 use strum::VariantArray;
 use strum_macros::{IntoStaticStr, VariantArray};
-use unicode_reader::CodePoints;
 
 use crate::TmpResult;
 
 pub struct Tokenizer {
-    chars: PeekNth<Characters>,
+    iter: MultiPeek<MmapReader>,
     line: u32,
     column: u32,
-    peek_index: u32
 }
 
 impl Tokenizer {
     pub fn new(file: impl AsRef<Path>) -> TmpResult<Tokenizer> {
-        Ok(Tokenizer { chars: read_chars(file)?, line: 1, column: 1, peek_index: 0 })
+        Ok(Tokenizer {
+            iter: Self::open_file(file)?,
+            line: 1,
+            column: 1,
+        })
     }
 
-    /// Peeks the found whitespace and the token at the end of it if found.
-    /// Whitespace after tokens is not included. 
-    pub fn peek(&mut self) -> (Option<Token>, Whitespace) {
-        let mut whitespace = Whitespace::EMPTY;
+    pub fn next_token(&mut self) -> (Option<Token>, Whitespace) {
+        let mut whitespace = Whitespace::default();
         whitespace += self.skip_whitespace();
-        let Some(first_char) = self.peek_index() else { return (None, whitespace) };
-        if let Ok(symbol) = Symbol::try_from_primitive(first_char as u8) {
+        self.iter.reset_peek();
+        let Some(first_char) = self.iter.peek().cloned() else {
+            return (None, whitespace);
+        };
+        if let Ok(Ok(symbol)) = u8::try_from(first_char).map(Symbol::try_from_primitive) {
+            self.iter.next();
             if let Some(token) = self.parse_comment(symbol) {
                 return (Some(token), whitespace);
             }
-            self.peek_index +=1;
             return (Some(Token::Symbol(symbol)), whitespace);
         }
 
@@ -44,24 +48,39 @@ impl Tokenizer {
         }
 
         if !first_char.is_ascii_alphanumeric() {
-            panic!("Syntax Error: Invalid symbol:'{first_char}'")
+            let line = self.line + whitespace.new_lines;
+            let column = if whitespace.new_lines > 0 {
+                0
+            } else {
+                self.column
+            } + whitespace.columns;
+            eprintln!("Syntax Error: Invalid symbol: '{first_char}'@{line}:{column}");
+            exit(-1);
         }
         let identifier = self.parse_identifier();
         (Some(Token::Identifier(identifier)), whitespace)
     }
 
-    fn peek_index(&mut self) -> Option<char> {
-        self.chars.peek_nth(self.peek_index as usize).copied()
+    fn open_file(filename: impl AsRef<Path>) -> TmpResult<MultiPeek<MmapReader>> {
+        let file = File::open(filename)?;
+        let mmap =  unsafe { MmapOptions::new().populate().map(&file)? };
+        #[cfg(unix)]
+        mmap.advise(memmap2::Advice::Sequential)?;
+        //let reader = io::BufReader::new(file);
+        Ok(MmapReader::new(mmap).multipeek())
     }
 
     fn skip_whitespace(&mut self) -> Whitespace {
-        let mut whitespace = Whitespace::EMPTY;
-        while let Some(character) = self.peek_index() {
-            if !character.is_whitespace() { break; }
-            self.peek_index+=1;
+        self.iter.reset_peek();
+        let mut whitespace = Whitespace::default();
+        while let Some(character) = self.iter.peek().copied() {
+            if !character.is_whitespace() {
+                break;
+            }
+            self.iter.next();
             whitespace.columns += 1;
-            if character == '\n' { 
-                whitespace.new_lines += 1; 
+            if character == '\n' {
+                whitespace.new_lines += 1;
                 whitespace.columns = 0;
             }
         }
@@ -69,11 +88,16 @@ impl Tokenizer {
     }
 
     fn parse_identifier(&mut self) -> String {
+        self.iter.reset_peek();
         let mut identifier = String::new();
-        while let Some(character) = self.peek_index() {
-            if character.is_whitespace() { break;}
-            if Symbol::try_from_primitive(character as u8).is_ok() {break;}
-            self.peek_index+=1;
+        while let Some(character) = self.iter.peek().copied() {
+            if character.is_whitespace() {
+                break;
+            }
+            if Symbol::try_from_primitive(character as u8).is_ok() {
+                break;
+            }
+            self.iter.next();
             identifier.push(character);
         }
 
@@ -81,38 +105,92 @@ impl Tokenizer {
     }
 
     fn parse_keyword(&mut self) -> Option<Keyword> {
-        let mut string = String::new();
-        for i in 0..*KEYWORD_MAX_LEN {
-            if let Some(character) = self.chars.peek_nth(i + self.peek_index as usize) {
-                string.push(*character);
+        self.iter.reset_peek();
+        let mut keyword = String::new();
+        while let Some(character) = self.iter.peek().copied() {
+            if !character.is_alphabetic() {
+                break;
             }
+            keyword.push(character);
         }
-        let (name, keyword) = KEYWORD_MAP.iter().find(|(k,_)| string.starts_with(*k))?;
-        self.peek_index += name.len() as u32;
-        Some(*keyword)
+        let len = keyword.len();
+        let keywords = KEYWORD_LEN_TABLE.get(&(len as u8))?;
+        let keyword = keywords.get(&*keyword).copied();
+        if keyword.is_some() {
+            self.iter.nth(len-1);
+        }
+        keyword
+
     }
 
     fn parse_comment(&mut self, first_symbol: Symbol) -> Option<Token> {
-        if !matches!(first_symbol, Symbol::Slash) {return None;}
-        let second_char = *self.chars.peek_nth(self.peek_index as usize + 1)?;
+        if !matches!(first_symbol, Symbol::Slash) {
+            return None;
+        }
+        let second_char = self.iter.next()?;
         let second_symbol = Symbol::try_from_primitive(second_char as u8).ok()?;
-        self.peek_index +=2;
         let comment = match second_symbol {
             Symbol::Slash => self.parse_line_comment(),
             Symbol::Asterisk => self.parse_block_comment(),
-            _ => return None
+            _ => return None,
         };
         Some(comment)
     }
 
     fn parse_line_comment(&mut self) -> Token {
+        self.iter.reset_peek();
+        if let Some(third_char) = self.iter.peek().copied() 
+                && let Some(third_symbol) = Symbol::try_from_primitive(third_char as u8).ok() 
+                && matches!(third_symbol, Symbol::Slash) {
+            self.iter.next();
+            return self.parse_doc_comment();
+        }
+        
+
         let mut comment = String::from("//");
-        while let Some(character) = self.peek_index() {
-            self.peek_index+=1;
-            if character == '\n' {break;}
+        for character in self.iter.by_ref() {
+            if character == '\n' {
+                break;
+            }
             comment.push(character);
         }
-        Token::Comment(Comment::LineComment(comment))
+        Token::Comment(Comment::Line(comment))
+    }
+
+    fn parse_doc_comment(&mut self) -> Token {
+        self.iter.reset_peek();
+        let mut comment = String::from("///");
+        let mut newlines = 0;
+        let mut found_newline = false;
+        let mut slash_count = 0;
+        while let Some(character) = self.iter.peek().copied() {
+            comment.push(character);
+
+            match character {
+                '\n' => {
+                    newlines+=1;
+                    found_newline = true;
+                    slash_count = 0;
+                }
+                '/' if found_newline => {
+                    slash_count += 1;
+                    if slash_count == 3 {
+                        slash_count = 0;
+                        found_newline = false;
+                    }
+                }
+                c if c.is_whitespace() => {},
+                _ if found_newline => {
+                    comment.pop();
+                    break;
+                }
+                 
+                _ => {}
+            }
+            self.iter.next();
+        }
+        self.iter.reset_peek();
+        Token::Comment(Comment::Doc(comment, newlines))
     }
 
     fn parse_block_comment(&mut self) -> Token {
@@ -120,24 +198,30 @@ impl Tokenizer {
         let mut new_line_count = 0;
         let mut columns = 0;
         let mut found_asterisk = false;
-        while let Some(character) = self.peek_index() {
-            self.peek_index+=1;
+        for character in self.iter.by_ref() {
             comment.push(character);
-            columns+=1;
-            if character == '\n' { 
-                new_line_count+=1;
-                columns = 0;
-            };
-            if character == '*' {
-                found_asterisk = true; 
-                continue;
+            columns += 1;
+
+            match character {
+                '\n' => {
+                    new_line_count += 1;
+                    columns = 0;
+                }
+                '*' => {
+                    found_asterisk = true;
+                    continue;
+                }
+                '/' if found_asterisk => break,
+                _ => {}
             }
-            if !found_asterisk {continue;}
-            if character == '/' {break;}
             found_asterisk = false;
         }
 
-        Token::Comment(Comment::BlockComment {comment, new_line_count, columns})
+        Token::Comment(Comment::Block {
+            comment,
+            new_line_count,
+            columns,
+        })
     }
 }
 
@@ -145,14 +229,18 @@ impl Iterator for Tokenizer {
     type Item = PosToken;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let (token, whitespace) = self.peek();
+        let (token, whitespace) = self.next_token();
         self.line += whitespace.new_lines;
         self.column += whitespace.columns;
         if whitespace.new_lines > 0 {
             self.column = whitespace.columns + 1;
         }
         let token = token?;
-        let pos_token = PosToken {token, line: self.line, column: self.column };
+        let pos_token = PosToken {
+            token,
+            line: self.line,
+            column: self.column,
+        };
         self.line += pos_token.token.new_lines();
         self.column += pos_token.token.line_len();
         if pos_token.token.new_lines() > 0 {
@@ -163,15 +251,11 @@ impl Iterator for Tokenizer {
     }
 }
 
-type Characters = FilterMap<CodePoints<Bytes<io::BufReader<File>>>, fn (Result<char, io::Error>) -> Option<char>>;
-
-fn read_chars(filename: impl AsRef<Path>) -> io::Result<PeekNth<Characters>> {
-    let file = File::open(filename)?;
-    let reader = io::BufReader::new(file);
-    let characters: Characters = CodePoints::from(reader.bytes()).filter_map(Result::ok);
-    
-    Ok(itertools::peek_nth(characters))
-    
+#[derive(Debug)]
+pub struct PosToken {
+    pub token: Token,
+    pub line: u32,
+    pub column: u32,
 }
 
 #[derive(Debug)]
@@ -185,49 +269,57 @@ pub enum Token {
 impl Token {
     pub fn line_len(&self) -> u32 {
         match &self {
-            Token::Comment(Comment::LineComment(_)) => 0,
-            Token::Comment(Comment::BlockComment { columns, .. }) => *columns,
+            Token::Comment(Comment::Line(_)) => 0,
+            Token::Comment(Comment::Doc(..)) => 0,
+            Token::Comment(Comment::Block { columns, .. }) => *columns,
             Token::Identifier(identifier) => identifier.len() as u32,
             Token::Keyword(keyword) => <&str>::from(keyword).len() as u32,
-            Token::Symbol(_) => 1
+            Token::Symbol(_) => 1,
         }
     }
 
     fn new_lines(&self) -> u32 {
         match &self {
-            Token::Comment(Comment::LineComment(_)) => 1,
-            Token::Comment(Comment::BlockComment{new_line_count, ..}) => *new_line_count, 
-            _ => 0
+            Token::Comment(Comment::Line(_)) => 1,
+            Token::Comment(Comment::Block { new_line_count, .. }) => *new_line_count,
+            _ => 0,
         }
     }
 }
 
 lazy_static! {
-    static ref KEYWORD_MAP:BTreeMap<&'static str, Keyword> = Keyword::VARIANTS.iter().map(|keyword| (<&'static str>::from(keyword), *keyword)).collect();
-    static ref KEYWORD_MAX_LEN:usize =KEYWORD_MAP.keys().max_by_key(|name| name.len()).expect("unreachable!").len();
+    static ref KEYWORD_LEN_TABLE: HashMap<u8, HashMap<&'static str, Keyword>> = Keyword::VARIANTS 
+        .iter()
+        .copied()
+        .chunk_by(|k| <&'static str>::from(*k).len() as u8)
+        .into_iter()
+        .map(|(k,g)| (k, g.map(|keyword| (<&'static str>::from(keyword), keyword)).collect()))
+        .collect();
 }
 
 #[derive(Debug)]
 pub enum Comment {
-    LineComment(String),
-    BlockComment {
-        comment:String,
-        new_line_count:u32,
-        columns:u32,
-    }
+    Line(String),
+    Doc(String, u32),
+    Block {
+        comment: String,
+        new_line_count: u32,
+        columns: u32,
+    },
 }
 
 impl Comment {
     pub fn comment(&self) -> &str {
         match self {
-            Comment::BlockComment{ comment, ..} => comment,
-            Comment::LineComment(comment) => comment
+            Comment::Block { comment, .. } => comment,
+            Comment::Doc(comment, _) => comment,
+            Comment::Line(comment) => comment,
         }
     }
 }
 
 #[derive(IntoStaticStr, VariantArray, Clone, Copy, Debug)]
-#[strum(serialize_all="lowercase")]
+#[strum(serialize_all = "lowercase")]
 pub enum Keyword {
     Var,
     Fn,
@@ -286,38 +378,27 @@ pub enum Symbol {
     GreaterThen = b'>',
     SmallerThen = b'<',
     Hat = b'^',
-    Degree = b'\xB0',// "°" ASCII character
+    Degree = b'\xB0', // "°" ASCII character
     Backslash = b'\\',
     Percent = b'%',
     Dollar = b'$',
-    At = b'@'
+    At = b'@',
 }
 
-impl Symbol {
-    pub const fn char_equals(self, character:char) -> bool {
-        self as u8 == character as u8
-    }
-}
-#[derive(Debug)]
-pub struct PosToken {
-    pub token: Token,
-    pub line: u32,
-    pub column: u32,
-}
-
-
-
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Default)]
 pub struct Whitespace {
-    new_lines:u32,
-    columns:u32
+    new_lines: u32,
+    columns: u32,
 }
 
 impl Add for Whitespace {
     type Output = Whitespace;
 
     fn add(self, rhs: Self) -> Self::Output {
-        Whitespace { new_lines: self.new_lines+rhs.new_lines, columns: self.columns + rhs.columns }
+        Whitespace {
+            new_lines: self.new_lines + rhs.new_lines,
+            columns: self.columns + rhs.columns,
+        }
     }
 }
 
@@ -327,6 +408,45 @@ impl AddAssign for Whitespace {
     }
 }
 
-impl Whitespace {
-    const EMPTY:Whitespace = Whitespace {new_lines: 0, columns: 0 };
+pub struct MmapReader {
+    file:Mmap,
+    byte_index:usize
+}
+
+impl MmapReader {
+
+    pub fn new(file: Mmap) -> Self {
+        Self { file, byte_index: 0 }
+    }
+
+    fn utf8_to_char(slice:&[u8]) -> Result<char, UTF8ReadError> {
+        let chunk = slice.utf8_chunks().next().ok_or(UTF8ReadError::Eof)?;
+        chunk.valid().chars().next().ok_or(UTF8ReadError::InvalidByte)
+    }
+
+
+}
+
+impl Iterator for MmapReader {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let end_index = (self.file.len() - self.byte_index).clamp(0, 4) + self.byte_index;
+            let next_slice = self.file.get(self.byte_index..end_index)?;
+            match Self::utf8_to_char(next_slice) {
+                Ok(char) => {
+                    self.byte_index+=char.len_utf8();
+                    return Some(char)
+                },
+                Err(UTF8ReadError::Eof) => return None,
+                Err(UTF8ReadError::InvalidByte) => self.byte_index+=1
+            }
+        }
+    }
+}
+
+enum UTF8ReadError {
+    Eof,
+    InvalidByte
 }
